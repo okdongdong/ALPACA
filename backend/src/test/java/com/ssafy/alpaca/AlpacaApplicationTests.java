bd109ae8aa5d60f835d037eb1aacca9b7dac8239
양지훈 <kelvin9149@gmail.com>
2022-04-16 00:06:46 +0900

    S06P31E106-63 Feat: project init

---
+++ backend/src/test/java/com/ssafy/alpaca/AlpacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.alpaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
