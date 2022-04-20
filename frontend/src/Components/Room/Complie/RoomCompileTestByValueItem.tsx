a0a4781c8538f828ca5ead2d78860a2883bb22e7
성아영 <hablobien@naver.com>
2022-04-20 12:50:50 +0900

    S06P31E106-71 Feat: 코드 컴파일 창 생성

--- frontend/src/Components/Room/Complie/RoomCompileTestByValueItem.tsx
+++ frontend/src/Components/Room/Complie/RoomCompileTestByValueItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from './RoomCompileTestByValueItem.module.css';
 type testProps = {
   idx: number;
   inputValue: string;
@@ -10,19 +10,35 @@ type testProps = {
 function RoomCompileTestByValueItem({ idx, inputValue, outputValue, result }: testProps) {
   return (
     <div style={{ padding: '1.5rem' }}>
-      <div>테스트 {idx}</div>
-      <div>
-        <span style={{ paddingLeft: '2rem', paddingRight: '1rem' }}>입력값</span>
-        <span>{inputValue}</span>
-      </div>
-      <div>
-        <span style={{ paddingLeft: '2rem', paddingRight: '1rem' }}>출력값</span>
-        <span>{outputValue}</span>
-      </div>
-      <div>
-        <span style={{ paddingLeft: '2rem', paddingRight: '1rem' }}>실행결과</span>
-        <span>{result}</span>
-      </div>
+      <tbody>
+        <tr>
+          <td colSpan={2}>테스트 {idx}</td>
+        </tr>
+        <tr>
+          <td className={styles.td_label} align="right">
+            입력값 <span>〉</span>
+          </td>
+          <td className="input">{inputValue}</td>
+        </tr>
+        <tr>
+          <td className={styles.td_label} align="right">
+            기댓값 <span>〉</span>
+          </td>
+          <td className="output">{outputValue}</td>
+        </tr>
+        <tr>
+          <td className={styles.td_label} align="right" valign="top">
+            실행 결과 <span>〉</span>
+          </td>
+          <td className="result failed">{result}</td>
+        </tr>
+        <tr style={{ display: 'none' }}>
+          <td className={styles.td_label} align="right" valign="top">
+            출력 <span>〉</span>
+          </td>
+          <td className="stdout"></td>
+        </tr>
+      </tbody>
     </div>
   );
 }
