79bfcdefa2e7f971a22883c9a5dd849ba154ff06
roywnsdud <roywnsdud@naver.com>
2022-04-28 15:08:35 +0900

    S06P31E106-189 Feat: 프로필사진 변경 및 기본프로필사진 설정

--- frontend/src/Components/Main/MainIntroductionProfile.tsx
+++ frontend/src/Components/Main/MainIntroductionProfile.tsx
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
-import ProfileImg from '../../Assets/Img/Img.png';
 import styles from './MainIntroductionProfile.module.css';
 import EditProfile from '../Dialogs/EditProfile';
+import { useDispatch } from 'react-redux';
+import { setTheme } from '../../Redux/themeReducer';
+import { customAxios } from '../../Lib/customAxios';
+import { useSelector } from 'react-redux';
+import { setUserInfo } from '../../Redux/accountReducer';
+import { useNavigate } from 'react-router-dom';
 
 export interface ProfileProps {
   callback: Function;
@@ -35,15 +40,15 @@ const CIconButton = styled(IconButton)(({ theme }) => ({
 }));
 
 function MainIntroductionProfile(props: ProfileProps) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userInfo = useSelector((state: any) => state.account);
   const [open, setOpen] = React.useState(false);
   const [userData, setUserData] = useState<any[] | null>();
   const EditUserData = (sendData: any) => {
     setUserData(sendData);
     props.callback(userData);
   };
-  // const EditUserData = (sendData: any) => {
-  //   props.callback(sendData);
-  // };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -54,7 +59,7 @@ function MainIntroductionProfile(props: ProfileProps) {
 
   return (
     <ProfileDiv>
-      <img src={ProfileImg} className={styles.profileimg} alt="" />
+      <img src={userInfo.profileImg} className={styles.profileimg} alt="" />
       <CIconButton aria-label="EditIcon" onClick={handleClickOpen}>
         <EditIcon
           sx={{
