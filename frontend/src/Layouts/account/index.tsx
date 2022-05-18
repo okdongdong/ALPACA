7c2842a77200cfb8c7d184f4864e0b3e1cd64537
roywnsdud <roywnsdud@naver.com>
2022-05-19 01:16:24 +0900

    Fix: pin hover 추가

--- frontend/src/Layouts/account/index.tsx
+++ frontend/src/Layouts/account/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { BrowserView, MobileView } from 'react-device-detect';
+
 const RootStyle = styled('div')({
   height: '100%',
   display: 'flex',
