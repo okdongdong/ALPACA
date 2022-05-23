ee12b74d896e71264e068cfea896d30b23b36966
성아영 <hablobien@naver.com>
2022-05-23 17:33:54 +0900

    Fix: 스크린 공유 기본설정 수정

--- frontend/src/Pages/Room/Codes.tsx
+++ frontend/src/Pages/Room/Codes.tsx
@@ -73,6 +73,7 @@ function Codes() {
     if (!problemId) return;
 
     try {
+      console.log(studyId);
       const params = studyId
         ? {
             problemNumber: parseInt(problemId),
